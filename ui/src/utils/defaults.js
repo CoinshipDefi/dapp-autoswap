25784f9fea640e965f6f16ce6880448e442f2ba2
Pycomet <Codefred>
2021-03-09 02:27:36 +0100

    fix lint err

--- ui/src/utils/defaults.js
+++ ui/src/utils/defaults.js
@@ -1,9 +1,10 @@
-  // GENERATED FROM /Users/michael/agoric/dapp-autoswap/api/deploy.js
+  // GENERATED FROM /home/codefred/Documents/Glibx/dapp-autoswap/api/deploy.js
   export default {
-  "INSTANCE_BOARD_ID": "1202180815",
+  "INSTANCE_BOARD_ID": "605437304",
   "INVITATION_BRAND_BOARD_ID": "1667979430",
   "BRIDGE_URL": "http://127.0.0.1:8000",
   "API_URL": "http://127.0.0.1:8000",
-  "INSTALLATION_BOARD_ID": "1265655452",
+  "INSTALLATION_BOARD_ID": "813441138",
   "CONTRACT_NAME": "autoswap"
 };
+  
